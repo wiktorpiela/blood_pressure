88b948880b9596b5151189ae97e346c2b1d6d009
goesoftwarePY <wpiela9@gmail.com>
2023-11-14 07:46:10 +0100

    index page styling part1

--- blood_pressure/core/views.py
+++ blood_pressure/core/views.py
@@ -2,12 +2,17 @@ from typing import Any
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from .models import BloodPressure
+from django.db.models import Avg
 
 class Index(TemplateView):
-    data = BloodPressure.objects.all()
+    blood = BloodPressure
+    data = blood.objects.all()
+    avg_systolic = blood.objects.aggregate(Avg("systolic"))["systolic__avg"]
+    avg_diastolic = blood.objects.aggregate(Avg("diastolic"))["diastolic__avg"]
+    avg_hearth_rate = blood.objects.aggregate(Avg("hearth_rate"))["hearth_rate__avg"]
     template_name = "index.html"
 
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super(Index, self).get_context_data(**kwargs)
-        context.update({"data": self.data, "len":len(self.data)})
+        context.update({"data": self.data, "len":len(self.data), "avg_sys":self.avg_systolic, "avg_dia":self.avg_diastolic, "avg_hr":self.avg_hearth_rate})
         return context
