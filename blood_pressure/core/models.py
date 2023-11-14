44d9e5a0a8a66e62db9b1a13ec4a2a057aca6ac8
goesoftwarePY <wpiela9@gmail.com>
2023-11-14 06:13:02 +0100

    template view index added, model created

--- blood_pressure/core/models.py
+++ blood_pressure/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class BloodPressure(models.Model):
+    systolic = models.IntegerField()
+    diastolic = models.IntegerField()
+    hearth_rate = models.IntegerField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.timestamp
--- blood_pressure/core/urls.py
+++ blood_pressure/core/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.)
+    path("", views.Index.as_view(), name="index"),
 ]
