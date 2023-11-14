555d8f2dabdcf4941a6f611efcd6735d2e254d6e
goesoftwarePY <wpiela9@gmail.com>
2023-11-14 06:22:16 +0100

    index view render data on template

--- blood_pressure/core/views.py
+++ blood_pressure/core/views.py
@@ -1,5 +1,13 @@
+from typing import Any
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import BloodPressure
 
 class Index(TemplateView):
+    data = BloodPressure.objects.all()
     template_name = "index.html"
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super(Index, self).get_context_data(**kwargs)
+        context.update({"data": self.data, "len":len(self.data)})
+        return context
