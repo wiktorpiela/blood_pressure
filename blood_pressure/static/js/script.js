75d5c6f98b683537024fb542dd5188df85dbf784
goesoftwarePY <wpiela9@gmail.com>
2023-11-15 17:24:40 +0100

    script js for modal popup form added

---
+++ blood_pressure/static/js/script.js
@@ -0,0 +1,11 @@
+const popup = document.querySelector(".popup")
+const addBtn = document.querySelector(".addBtn")
+const cancelBtn = document.querySelector(".cancelBtn")
+
+addBtn.addEventListener("click", ()=>{
+    popup.classList.add("open-popup")
+})
+
+cancelBtn.addEventListener("click", ()=>{
+    popup.classList.remove("open-popup")
+})
